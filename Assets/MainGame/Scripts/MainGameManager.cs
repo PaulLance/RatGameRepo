225e9754a0446ad00460867ae60601be1171937c
coderx3 <gengargames@hotmail.com>
2022-09-25 14:07:57 +0200

    made main game scene look better and added leave button

---
+++ Assets/MainGame/Scripts/MainGameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class MainGameManager : MonoBehaviourPunCallbacks
+{
+    #region Photon Callbacks
+
+
+    /// <summary>
+    /// Called when the local player left the room. We need to load the launcher scene.
+    /// </summary>
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    #endregion
+
+
+    #region Public Methods
+
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+
+    #endregion
+}
